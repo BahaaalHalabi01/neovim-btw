f02c39bd8c564ebcc6faebce97ecb4049697f8f3
bahaa <bahaa.alhalabi01@mail.ru>
2024-10-27 22:31:33 +0300

    transparent bg

--- lua/halabi/lazy/theme.lua
+++ lua/halabi/lazy/theme.lua
@@ -18,16 +18,16 @@ return {
       invert_signs = false,
       invert_tabline = false,
       invert_intend_guides = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
+      inverse = true,  -- invert background for search, diffs, statuslines and errors
+      contrast = "",   -- can be "hard", "soft" or empty string
       palette_overrides = {},
       overrides = {},
       dim_inactive = false,
-      transparent_mode = false,
+      transparent_mode = true,
     })
 
-    vim.o.background = 'dark'
     vim.cmd([[colorscheme gruvbox]])
+    vim.o.background = 'dark'
     vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
   end
 }
